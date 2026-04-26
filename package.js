e6997123f09627d4beb04c7e22a59aba5c695ed9
fedeezframer <federico.martinez.212@mi.unc.edu.ar>
2026-04-26 14:31:45 -0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "negosocio-backend",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "googleapis": "^126.0.0",
+    "mercadopago": "^2.0.0",
+    "node-fetch": "^3.3.0"
+  }
+}
